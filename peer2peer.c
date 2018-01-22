ab8ffc0e62b9e2e3b5bb1f6bef1e0bf9756d7744
Lodur <fcs4200@gmail.com>
2018-01-22 15:09:59 -0200

    Code refactor

--- peer2peer.c
+++ peer2peer.c
@@ -4,11 +4,8 @@
 
 void main(int argc, char* argv[]) {
 
-
-int p, rank, val,destRank,numProcs;
- // int numNodes = 5; 
+ int p, rank, val,destRank,numProcs; 
  int msg;
-
 
  MPI_Status status;
  MPI_Init(&argc, &argv) ;
@@ -16,27 +13,25 @@ int p, rank, val,destRank,numProcs;
  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
  int ratio = 10000/(numProcs-1);
 
-
-if ( rank == 0){
+if (rank == 0){
    int x;
    int y = 1;
    destRank = 1;
    for (x=rank+1; x < numProcs; x++){
         for (y = x*ratio; y >= (x*ratio)-ratio; y--){
                 MPI_Send(&y, 1, MPI_INT, x, TAG, MPI_COMM_WORLD);
-                //if ( y % 1000 == 0)
-                printf("0 enviando %d para %d \n", y, x);
+                //printf("0 sending message %d to %d \n", y, x);
         }
    }
+ 
    int totalSum = 0;
    for(x=1; x<numProcs; x++){
         MPI_Recv(&msg, 1, MPI_INT, x, TAG, MPI_COMM_WORLD,&status);
-        printf("Soma recebida de %d = %d \n", x, msg);
+        printf("Received sum: %d = %d \n", x, msg);
         totalSum += msg;
    }
-
-   printf("Soma total = %d", totalSum);
-   //printf("Process %d sends a message to %d\n",rank,i);
+   printf("Total Sum = %d", totalSum);
+ 
  } else {
        int z;
        int sum;
@@ -47,8 +42,7 @@ if ( rank == 0){
         }
 
         MPI_Send(&sum, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
-        // receber o valor enviado
-        // printf("Process %d receives a message from %d with value\n",rank,0,val);
+        //printf("Process %d receives a message from %d with value\n",rank,0,val);
  }
    MPI_Finalize();
 }
